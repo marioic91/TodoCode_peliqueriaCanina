c0b0c9eb6058473fe443d48c9c55782a89139505
Mario Ivan Carreño <128433874+marioic91@users.noreply.github.com>
2024-09-23 08:14:45 -0300

    Add files via upload

---
+++ PeluqueriaCanina/target/generated-sources/annotations/com/mycompany/peluqueriacanina/logica/Dueno_.java
@@ -0,0 +1,15 @@
+package com.mycompany.peluqueriacanina.logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-09-23T08:04:44", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Dueno.class)
+public class Dueno_ { 
+
+    public static volatile SingularAttribute<Dueno, String> celular;
+    public static volatile SingularAttribute<Dueno, Integer> id;
+    public static volatile SingularAttribute<Dueno, String> nombre;
+
+}
